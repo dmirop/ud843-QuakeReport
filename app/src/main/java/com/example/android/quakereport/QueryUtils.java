a4a7fa027a7e98fb1c9558878916c35b409701dd
dmirop <daniel.miro.pettican@gmail.com>
2017-08-12 18:14:44 +0200

    1.18

--- app/src/main/java/com/example/android/quakereport/QueryUtils.java
+++ app/src/main/java/com/example/android/quakereport/QueryUtils.java
@@ -1,89 +1,18 @@
 package com.example.android.quakereport;
 
-import android.text.TextUtils;
 import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.List;
-
-import static com.example.android.quakereport.EarthquakeActivity.LOG_TAG;
 
 /**
  * Helper methods related to requesting and receiving earthquake data from USGS.
  */
 
 public final class QueryUtils {
-
-    private static URL createUrl(String stringUrl){
-        URL url = null;
-        try {
-            url = new URL(stringUrl);
-        } catch (MalformedURLException e) {
-            Log.e(LOG_TAG, "Problem building the URL ", e);
-        }
-
-        return url;
-    }
-
-    private static String makeHttpRequest(URL url) throws IOException{
-        String jsonResponse = "";
-
-        if (url == null) {return jsonResponse;}
-
-        HttpURLConnection urlConnection = null;
-        InputStream inputStream = null;
-        try {
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setReadTimeout(10000);
-            urlConnection.setConnectTimeout(15000);
-            urlConnection.setRequestMethod("GET");
-            urlConnection.connect();
-
-            if (urlConnection.getResponseCode() == 200){
-                inputStream = urlConnection.getInputStream();
-                jsonResponse = readFromStream(inputStream);
-            } else {
-                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
-            }
-        } catch (IOException e){
-            Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
-        } finally {
-            if (urlConnection != null){
-                urlConnection.disconnect();
-            }
-            if (inputStream != null){
-                inputStream.close();
-            }
-        }
-        return jsonResponse;
-    }
-
-    private static String readFromStream(InputStream inputStream) throws IOException{
-        StringBuilder output = new StringBuilder();
-        if (inputStream != null){
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
-            BufferedReader reader = new BufferedReader(inputStreamReader);
-            String line = reader.readLine();
-            while (line != null){
-                output.append(line);
-                line = reader.readLine();
-            }
-        }
-
-        return output.toString();
-    }
 
     /**
      * Create a private constructor because no one should ever create a {@link QueryUtils} object.
